398d168048d4c72b4243db02ea7bf5d83140bc83
VasylArtymovych <92371486+VasylArtymovych@users.noreply.github.com>
2022-05-04 14:50:48 +0000

    Deploying to gh-pages from @ VasylArtymovych/goit-js-hw-11@c9eb4902364c7acb15b66cb4a17ead70b09cb008 🚀

--- src.e6342517.js
+++ src.e6342517.js
@@ -3,4 +3,4 @@ parcelRequire=function(e,r,t,n){var i,o="function"==typeof parcelRequire&&parcel
 },{}],"Focm":[function(require,module,exports) {
 "use strict";require("./css/styles.css");
 },{"./css/styles.css":"krre"}]},{},["Focm"], null)
-//# sourceMappingURL=/goit-js-hw-11/src.671babf5.js.map
+//# sourceMappingURL=/goit-js-hw-11/src.e6342517.js.map
